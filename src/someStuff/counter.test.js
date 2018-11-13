308bc1ab3cab19a6d2d36b629bedd769ddb58679
Sergey <sergey.bombin@intspirit.com>
2018-11-13 18:26:22 +0300

    Abramov. Lesson 5. Writing a Counter Reducer with Tests

---
+++ src/someStuff/counter.test.js
@@ -0,0 +1,21 @@
+function counter(state = 0, action){
+
+    switch (action.type){
+        case 'INCREMENT':
+            return state +1;
+        case 'DECREMENT':
+            return state -1
+        default:
+            return state  
+    }
+}
+
+it('test counter', function () {
+    expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+    expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+    expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+    expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+    expect(counter(1, { type: 'UNKNOWN ACTION' })).toEqual(1);
+    expect(counter(undefined, { type: 'INCREMENT' })).toEqual(1);
+})
+
