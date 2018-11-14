5144898a611596d8072a0c42151859efdbe35e9d
Sergey <sergey.bombin@intspirit.com>
2018-11-14 15:57:24 +0300

    simulate button onClick

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,13 +1,47 @@
+import * as React from 'react';
 import { createStore } from 'redux'
 import counter from '../reducers/counter'
+import Counter from './Counter'
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('`Counter` component testing', () => {
-
-    it('First test', () => {
+    
+    it('Reducer test', () => {
         // create store
         const store = createStore(counter)
         // initial store should be equal to 0
         expect(store.getState()).toBe(0)
+        // increment
+        store.dispatch({type: 'INCREMENT'})
+        expect(store.getState()).toBe(1)
+        // decrement
+        store.dispatch({type: 'DECREMENT'})
+        expect(store.getState()).toBe(0)
+        
     })
+
+    it('Button pressed test', () => {
+        // create store
+        const store = createStore(counter)
+        // initial store should be equal to 0
+        expect(store.getState()).toBe(0)
+
+        const wrapper = shallow(<Counter store={store}/>);
+
+        // button should exist
+        expect(wrapper.find('.button1').length).toBe(1)
+        
+        // increment button should store ++
+        wrapper.find('.button1').simulate('click') 
+        expect(store.getState()).toBe(1)
+
+        // decrement button should store --
+        wrapper.find('.button2').simulate('click') 
+        expect(store.getState()).toBe(0)
+
+    })    
 
 })
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -9,22 +9,22 @@ class Counter extends React.Component<any>{
                     <h1> Hello </h1>
                 </div>
                 <div>
-                    <button onClick ={this.onIncrement}>+</button>
-                    <h1>{this.props.store.getState()}</h1>
-                    <button onClick = {this.onDecrement}>-</button>
+                    <button className='button1' onClick={this.onIncrement}>+</button>
+                    <h1 className='counts'>{this.props.store.getState()}</h1>
+                    <button className='button2' onClick={this.onDecrement}>-</button>
                 </div>
             </div>
         )
     }
-    
+
     private onIncrement = () => {
         this.props.store.dispatch({ type: 'INCREMENT' })
         console.log(`increment callback was called. Current state: ${this.props.store.getState()}`)
-      }
+    }
     private onDecrement = () => {
         this.props.store.dispatch({ type: 'DECREMENT' })
         console.log(`decrement callback was called. Current state: ${this.props.store.getState()}`)
-      }    
+    }
 }
 
 export default Counter;
