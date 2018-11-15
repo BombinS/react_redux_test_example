fe89aaf17dd58b6db4f09cf62e5c0d06e1138c27
Sergey <sergey.bombin@intspirit.com>
2018-11-15 18:31:20 +0300

    Abramov. Lesson 13. Reducer arrays composition

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,20 +1,29 @@
+const todo = (state: any, action: any) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        case 'TOGGLE_TODO':
+            if (state.id != action.id)
+                return state
+            else
+                return { ...state, ...{ completed: !state.completed } }
+    }
+}
+
 const todos = (state = [], action: any) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
+                ...[todo(state, action)]
             ]
         case 'TOGGLE_TODO':
-            return state.map((todo: any) => {
-                if (todo.id != action.id)
-                    return todo
-                else
-                    return { ...todo, ...{ completed: !todo.completed } }
+            return state.map((t: any) => {
+                return todo(t, action)
             })
         default:
             return state
