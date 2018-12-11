3bd8bb30e5a200c79e07df329a05ee0265002c01
Sergey Bombin <sergey.bombin@intspirit.com>
2018-12-11 23:41:37 +0300

    Abramov. Lesson 18. React ToDo List Example (Toggle ToDo)

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -4,12 +4,12 @@ let nextToDoId = 0;
 let inputContent = '';
 
 export default class Todo extends React.Component<any> {
-  public inputFieldContent = '';  
+  public inputFieldContent = '';
   public render() {
     const todos = this.props.store.getState().todos;
     return (
       <div>
-        <input placeholder="todo value" onChange={this.handleInputOnChange}/>
+        <input placeholder="todo value" onChange={this.handleInputOnChange} />
         <button
           onClick={this.addTodo}
         >
@@ -18,15 +18,31 @@ export default class Todo extends React.Component<any> {
         <ul>
           {todos.map(
             (todo: any) =>
-              <li key={todo.id}>
+              <li
+                key={todo.id}
+                onClick={this.toggleTodo.bind(this, todo)}
+                style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              >
                 {todo.text}
-              </li>)}
+              </li>
+          )}
         </ul>
       </div>
     )
   }
 
-  private handleInputOnChange(e:any){
+  private toggleTodo = (todo: any): any => {
+    console.log(JSON.stringify(todo));
+    this.props.store.dispatch(
+      {
+        id: todo.id,
+        type: 'TOGGLE_TODO',
+      }
+    )
+    console.log(JSON.stringify(this.props.store.getState()));
+  }
+
+  private handleInputOnChange(e: any) {
     inputContent = e.target.value;
   }
 
